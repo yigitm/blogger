ffcb6039b57e85af04316922c128df5b84967101
Yigit Mersin <yigitmersin@gmail.com>
2022-03-22 23:09:47 +0300

    Update keywords in users controller

--- spec/controllers/posts_controller_spec.rb
+++ spec/controllers/posts_controller_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe 'Posts', type: :request do
- describe 'GET #index' do
+ context 'GET #index' do
    before(:example) { get posts_path }
    it "is a success" do
      expect(response).to have_http_status(:ok)
@@ -10,11 +10,11 @@ RSpec.describe 'Posts', type: :request do
    it "renders 'index' template" do
      expect(response).to render_template('index')
    end
- end
+  end
 end
 
 RSpec.describe 'Posts', type: :feature do
-  describe 'the posts page' do
+  context 'the posts page' do
     it 'should includes the correct placeholder text' do
       visit 'posts'
       expect(page).to have_text('List of posts - Root Page - action: posts#index')
--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe 'Users', type: :request do
- describe 'GET #index' do
+  context 'GET #index' do
    before(:example) { get users_path }
    it "is a success" do
      expect(response).to have_http_status(:ok)
@@ -10,9 +10,8 @@ RSpec.describe 'Users', type: :request do
    it "renders 'index' template" do
      expect(response).to render_template('index')
    end
- end
-
- describe 'GET #show' do
+  end
+  context 'GET #show' do
    before(:example) { get user_path(1) }
    it "is a success" do
      expect(response).to have_http_status(:ok)
@@ -21,18 +20,16 @@ RSpec.describe 'Users', type: :request do
    it "renders 'show' template" do
      expect(response).to render_template('show')
    end
- end
+  end
 end
 
 RSpec.describe 'Users', type: :feature do
-  describe 'the users page' do
+  context 'the users #index/#show pages' do
     it 'should includes the correct placeholder text' do
       visit 'users'
       expect(page).to have_text('List of Users - Root Page - action: users#index')
     end
-  end
-
-  describe 'the users#show page' do
+  
     it 'should includes the correct placeholder text' do
       visit 'users/1'
       expect(page).to have_text('Single User Page - action: users#show')
