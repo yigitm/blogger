522721a2f7a0eb347f83ae8156be254782f532e7
Yigit Mersin <yigitmersin@gmail.com>
2022-03-23 22:01:12 +0300

    Reset DB & re-create user model

--- db/schema.rb
+++ db/schema.rb
@@ -10,29 +10,9 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_03_23_164004) do
+ActiveRecord::Schema[7.0].define(version: 2022_03_23_160404) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
-
-  create_table "comments", force: :cascade do |t|
-    t.string "text"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
-  create_table "likes", force: :cascade do |t|
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
-  create_table "posts", force: :cascade do |t|
-    t.string "title"
-    t.string "text"
-    t.integer "comments_counter"
-    t.integer "likes_counter"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
 
   create_table "users", force: :cascade do |t|
     t.string "name"
