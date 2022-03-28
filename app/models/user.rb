1b602a665a31296ef687c7eceb154fdde83943aa
Yigit Mersin <yigitmersin@gmail.com>
2022-03-29 00:38:08 +0300

    Add 3 most recent posts method to users

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,8 @@ class User < ApplicationRecord
   has_many :comments, dependent: :destroy
   has_many :posts, dependent: :destroy
   has_many :likes, dependent: :destroy
+
+  def self.recent_users(user)
+    Post.where('author_id = ?', user.id).order(id: :desc).limit(3)
+  end
 end
