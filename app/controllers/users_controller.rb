736f62e9fcaa1a285806eb09d26e2f513bf68f4a
Yigit Mersin <yigitmersin@gmail.com>
2022-03-29 23:59:19 +0300

    Add style to index & show and add show page with bio section

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,5 +3,13 @@ class UsersController < ApplicationController
     @users = User.all
   end
 
-  def show; end
+  def show 
+    @user = User.find(params[:id])
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:id, :name, :photo, :bio, :post_counter)
+  end
 end
