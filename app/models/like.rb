354e5617f87798f7eef8b4fbc11ecb51dbfbee7f
Yigit Mersin <yigitmersin@gmail.com>
2022-03-29 03:22:06 +0300

    Add like counter method to Like model

--- app/models/like.rb
+++ app/models/like.rb
@@ -1,4 +1,9 @@
 class Like < ApplicationRecord
-  belongs_to :author
-  belongs_to :post
+  belongs_to :author, foreign_key: 'author_id'
+  belongs_to :post, foreign_key: 'post_id'
+
+  def self.like_counter(post)
+    value = post.likes_counter += 1
+    post.update(likes_counter: value)
+  end
 end
