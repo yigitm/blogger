3e13e5917aa00885bb35bdbd64b362e447c11752
Yigit Mersin <yigitmersin@gmail.com>
2022-03-30 22:42:19 +0300

    Add user post view

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,9 +1,14 @@
 class PostsController < ApplicationController
   def index
-    @user = User.find(params[:user_id])
+    @user = find_user
     @posts = Post.last(3)
   end
 
   def show
+    @post = find_user.posts.find(params[:id])
+  end
+
+  def find_user
+    User.find(params[:user_id])
   end
 end
