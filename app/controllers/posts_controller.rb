40c421013cb6c74ca4d81e70ef900e983677d9b7
Yigit Mersin <yigitmersin@gmail.com>
2022-03-31 03:34:51 +0300

    Add form for creating post

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -8,7 +8,32 @@ class PostsController < ApplicationController
     @post = find_user.posts.find(params[:id])
   end
 
+  def new
+    @user = find_user
+    @post = Post.new
+  end
+
+  def create
+    @user = find_user
+    @post = @user.posts.new(post_params)
+    @post.comments_counter = 0
+    @post.likes_counter = 0
+
+      if @post.save
+        redirect_to user_posts_path(@user)
+      else
+        flash[:alert] = 'Post can not created!'
+        render :new
+      end
+  end
+  
+
   def find_user
     User.find(params[:user_id])
   end
+
+  private
+    def post_params
+      params.require(:post).permit(:title, :text)
+    end
 end
