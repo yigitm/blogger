c2b838e522b4e65397c33f0543660614c1ce5579
Yigit Mersin <yigitmersin@gmail.com>
2022-04-01 18:33:11 +0300

    Add user name validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,8 @@ class User < ApplicationRecord
   has_many :posts, foreign_key: :author_id, dependent: :destroy
   has_many :likes, foreign_key: :author_id, dependent: :destroy
 
+  validates :name, presence: true
+
   def self.recent_posts(user)
     Post.where('author_id = ?', user.id).order(id: :desc).limit(3)
   end
