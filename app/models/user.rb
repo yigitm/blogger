bd5462cdbd648583a81c4dad32c00b9a19e87a21
Yigit Mersin <yigitmersin@gmail.com>
2022-04-01 19:17:56 +0300

    Add post_counter validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,7 @@ class User < ApplicationRecord
   has_many :likes, foreign_key: :author_id, dependent: :destroy
 
   validates :name, presence: true
+  validates :post_counter, numericality: { greater_than_or_equal_to: 0 }
 
   def self.recent_posts(user)
     Post.where('author_id = ?', user.id).order(id: :desc).limit(3)
