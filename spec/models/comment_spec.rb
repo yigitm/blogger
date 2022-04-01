5573418b5125ea55d9251311f8d457b2074d6b52
Yigit Mersin <yigitmersin@gmail.com>
2022-04-02 00:55:00 +0300

    Add like test & update comment test

--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 RSpec.describe Comment, type: :model do
   user = User.create(name: 'Test User', photo:'https://images.unsplash.com/photo-1508921912186-1d1a45ebb3c1?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80', bio: 'lorem ipsum', posts_counter: 23 ) 
-  post = Post.create(author_id: user.id, title: 'Test', text: 'Lorem Ipsum', comments_counter: 0, likes_counter: 0)
+  post = Post.create(author_id: user.id, title: 'Test', text: 'Lorem Ipsum', comments_counter: 1, likes_counter: 0)
   subject { Comment.new(text: 'Comment', author_id: user.id, post_id: post.id) }
 
   before { subject.save }
@@ -12,8 +12,11 @@ RSpec.describe Comment, type: :model do
     expect(subject).to_not be_valid
   end
 
-  it 'comments counter in post should be 2' do
-    expect(subject.comment_counter.comments_counter).to eq(2)
+  it 'comment should be valid' do
+    expect(subject).to be_valid
   end
 
+  it 'comments counter in post should be 1' do
+    expect(subject.post.comments_counter).to eq(2)
+  end
 end
--- spec/models/like_spec.rb
+++ spec/models/like_spec.rb
@@ -1,5 +1,17 @@
 require 'rails_helper'
 
 RSpec.describe Like, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  user = User.create(name: 'Test User', photo:'https://images.unsplash.com/photo-1508921912186-1d1a45ebb3c1?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80', bio: 'lorem ipsum', posts_counter: 23 ) 
+  post = Post.create(author_id: user.id, title: 'Test', text: 'Lorem Ipsum', comments_counter: 0, likes_counter: 0)
+  subject { Like.new(author_id: user.id, post_id: post.id) }
+
+  before { subject.save }
+
+  it 'likes counter in post should be 2' do
+    expect(subject.post.likes_counter).to eq(1)
+  end
+
+  it 'like should be valid' do
+    expect(subject).to be_valid
+  end
 end
