3492197279cf0ca597569f4ac523a53fc6c59952
Yigit Mersin <yigitmersin@gmail.com>
2022-04-05 06:14:20 +0300

    Addd logout button to user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,10 @@
 class UsersController < ApplicationController
+  before_action :authenticate_user!
+
   def index
   end
 
   def show
-    User.find(params[:id])
+      User.find(params[:id])
   end
 end
