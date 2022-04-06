14001c99870d6b90b4c1df3fd25419bd2fec2c41
Yigit Mersin <yigitmersin@gmail.com>
2022-04-06 18:46:38 +0300

    Update instance of the user index

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,9 @@
 class UsersController < ApplicationController
   before_action :authenticate_user!
 
-  def index; end
+  def index
+    @users = User.all
+  end
 
   def show
     User.find(params[:id])
